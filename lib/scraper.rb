648e97247f3eeb98e284497118855de440f99b49
DAZUN XING <dazun.xing@gmail.com>
2020-05-24 07:36:22 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,31 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+ 
+  def get_page 
+    url="http://learn-co-curriculum.github.io/site-for-scraping/courses"
+    doc=Nokogiri::HTML(open(url))
+    
+  end
+  
+  def get_courses
+    self.get_page.css(".post")
+  end
+  
+  def make_courses
+    self.get_courses.each do |element|
+      course =Course.new
+      course.title =element.css("h2").text
+      course.schedule =element.css(".date").text
+      course.description =element.css("p").text
+    end
+   
+  end
+  
   
   def print_courses
     self.make_courses
@@ -17,6 +39,8 @@ class Scraper
   end
   
 end
+
+Scraper.new.print_courses
 
 
 
