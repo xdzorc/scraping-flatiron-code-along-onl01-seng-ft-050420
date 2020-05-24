88cb57083fe236b16f44a71370e9e003db65b2e0
DAZUN XING <dazun.xing@gmail.com>
2020-05-24 06:33:23 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,11 +1,16 @@
 class Course 
    attr_accessor :title, :schedule, :description
    @@all=[]
-   def self.all 
+  def initialize
+    @@all << self
+  end
+  
+  def self.all 
      @@all
-   end
-   def self.reset_all
+  end
+  
+  def self.reset_all
      @@all.clear
-   end
+  end
 end
 
