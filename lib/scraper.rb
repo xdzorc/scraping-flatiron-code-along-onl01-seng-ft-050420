9fc933216f1ad596ae70d2dd342a91ba5a719d92
DAZUN XING <dazun.xing@gmail.com>
2020-05-24 07:29:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,8 +7,8 @@ require_relative './course.rb'
 class Scraper
  
   def get_page 
-    "http://learn-co-curriculum.github.io/site-for-scraping/courses"
-    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    url"http://learn-co-curriculum.github.io/site-for-scraping/courses"
+    doc=Nokogiri::HTML(open(url))
     
   end
   
